7956398b54ab2e87295c5005961a9e71775be143
fainz777 <fainz777@gmail.com>
2019-10-26 15:56:56 +0300

    init settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader']
+          })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({filename: 'style.css'}),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
